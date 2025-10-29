6ed49546dc6750cec3c2f3aebb01c6f640dd27f7
Nikita <nikita.ignatenko003@gmail.com>
2025-10-29 20:28:28 +0300

    mainwindow.cpp

---
+++ mainwindow.cpp
@@ -0,0 +1,167 @@
+#include "mainwindow.h"
+#include "startscreen.h"
+#include "ui_mainwindow.h"
+#include <QDialog>
+#include <QVBoxLayout>
+#include <QDialogButtonBox>
+#include <QListWidget>
+#include <QTimer>
+#include <QMessageBox>
+
+int MainWindow::kInstanceCount = 0;
+
+MainWindow::MainWindow(int userId, QString userName, std::shared_ptr<Database> dbPtr, QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow),
+    m_userId(userId),
+    m_userName(userName)
+{
+    ui->setupUi(this);
+    kInstanceCount++;
+
+    if(dbPtr)
+        m_dbPtr = dbPtr;
+    else
+        m_dbPtr = std::make_shared<Database>();
+
+    m_updateTimer = new QTimer(this);
+    connect(m_updateTimer, &QTimer::timeout, this, &MainWindow::updateChats);
+    m_updateTimer->start(1000);
+
+    setWindowTitle(QString("Chat Client - %1").arg(userName));
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    kInstanceCount--;
+    if(kInstanceCount <= 0)
+        qApp->exit(0);
+}
+
+MainWindow *MainWindow::createClient(std::shared_ptr<Database> dbPtr)
+{
+    StartScreen s(dbPtr);
+    auto result = s.exec();
+    if(result == QDialog::Rejected)
+    {
+        return nullptr;
+    }
+    auto w = new MainWindow(s.userId(), s.userName(), s.getDatabase());
+    w->setAttribute(Qt::WA_DeleteOnClose);
+    return w;
+}
+
+void MainWindow::on_messageLineEdit_returnPressed()
+{
+    on_sendMessageButton_clicked();
+}
+
+void MainWindow::on_sendMessageButton_clicked()
+{
+    if (m_dbPtr->isUserBanned(m_userName.toStdString())) {
+        QMessageBox::warning(this, tr("Warning"), tr("You are banned and cannot send messages"));
+        return;
+    }
+
+    if (!ui->messageLineEdit->text().isEmpty())
+    {
+        m_dbPtr->addChatMessage(m_userName.toStdString(),
+                              ui->messageLineEdit->text().toStdString());
+        ui->messageLineEdit->clear();
+    }
+}
+
+void MainWindow::on_privateMessageSendButton_clicked()
+{
+    if (m_dbPtr->isUserBanned(m_userName.toStdString())) {
+        QMessageBox::warning(this, tr("Warning"), tr("You are banned and cannot send messages"));
+        return;
+    }
+
+    QDialog dial(this);
+    dial.setModal(true);
+    auto l = new QVBoxLayout();
+    dial.setLayout(l);
+    auto userListWgt = new QListWidget(&dial);
+    l->addWidget(userListWgt);
+    auto buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok | QDialogButtonBox::Cancel, &dial);
+    l->addWidget(buttonBox);
+    connect(buttonBox, &QDialogButtonBox::accepted, &dial, &QDialog::accept);
+    connect(buttonBox, &QDialogButtonBox::rejected, &dial, &QDialog::reject);
+    auto userList = m_dbPtr->getUserList();
+    for(auto user : userList)
+    {
+        userListWgt->addItem(QString::fromStdString(user));
+    }
+    userListWgt->setCurrentRow(0);
+    auto result = dial.exec();
+    if(result == QDialog::Accepted && userListWgt->currentItem())
+    {
+        m_dbPtr->addPrivateMessage(m_userName.toStdString(),
+                                 userListWgt->currentItem()->text().toStdString(),
+                                 ui->messageLineEdit->text().toStdString());
+        ui->messageLineEdit->clear();
+    }
+}
+
+void MainWindow::on_actionOpen_another_client_triggered()
+{
+    auto w = createClient(m_dbPtr);
+    if(w)
+        w->show();
+}
+
+void MainWindow::on_actionCloseClient_triggered()
+{
+    this->close();
+}
+
+void MainWindow::updateChats()
+{
+    if (m_dbPtr->isUserBanned(m_userName.toStdString())) {
+        ui->sendMessageButton->setEnabled(false);
+        ui->privateMessageSendButton->setEnabled(false);
+        ui->messageLineEdit->setEnabled(false);
+        ui->statusbar->showMessage(tr("You are banned from the chat"));
+    } else {
+        ui->sendMessageButton->setEnabled(true);
+        ui->privateMessageSendButton->setEnabled(true);
+        ui->messageLineEdit->setEnabled(true);
+        ui->statusbar->showMessage(tr("Connected"));
+    }
+
+    auto chatMessages = m_dbPtr->getChatMessages();
+    QString chat;
+    for(const auto &msg : chatMessages)
+    {
+        chat.append(QString::fromStdString(msg) + "\n");
+    }
+    if(ui->commonChatBrowser->toPlainText() != chat)
+        ui->commonChatBrowser->setText(chat);
+
+    chat.clear();
+    auto privateMessages = m_dbPtr->getPrivateMessage(m_userId);
+    for(const auto &msg : privateMessages)
+    {
+        QString prefix;
+        if(m_userName == QString::fromStdString(msg.getSender()) &&
+           m_userId == msg.getDest())
+        {
+            prefix = tr("self message") + ": ";
+        }
+        else if(m_userName == QString::fromStdString(msg.getSender()))
+        {
+            prefix = tr("message to") +
+                    QString(" <%1>: ").arg(QString::fromStdString(m_dbPtr->getUserName(msg.getDest())));
+        }
+        else
+        {
+            prefix = "<" + QString::fromStdString(msg.getSender()) +
+                    "> " + tr("say to you") + ": ";
+        }
+        chat.append(prefix + QString::fromStdString(msg.getText()) + "\n");
+    }
+    if(ui->privateChatBrowser->toPlainText() != chat)
+        ui->privateChatBrowser->setText(chat);
+}
