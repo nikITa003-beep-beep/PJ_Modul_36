e9831c1b868649e0ce470c39018e2dec434cf323
Nikita <nikita.ignatenko003@gmail.com>
2025-10-29 20:34:14 +0300

    Test.cpp

---
+++ Test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <QApplication>
+#include "CommandLineInterface.h"
+#include "mainwindow.h"
+#include "serverwindow.h"
+#include <QTranslator>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    QTranslator myappTranslator;
+    myappTranslator.load("translations/my_ru.qm");
+    a.installTranslator(&myappTranslator);
+
+    QTranslator qtTranslator;
+    qtTranslator.load("translations/qt_ru.qm");
+    a.installTranslator(&qtTranslator);
+
+    auto dbPtr = std::make_shared<Database>();
+
+    auto w = MainWindow::createClient(dbPtr);
+    auto server = new ServerWindow(dbPtr);
+    server->show();
+
+    if(w)
+        w->show();
+    else
+        return 0;
+
+    return a.exec();
+}
